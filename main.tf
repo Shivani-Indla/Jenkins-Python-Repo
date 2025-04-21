6a2138315736d129e6c9852549b778832ad4fb0d
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-22 01:13:13 +0530

    My 3rd commit to create ec2-instance using jenkins pipeline with terraform code

--- main.tf
+++ main.tf
@@ -9,14 +9,3 @@ resource "aws_instance" "foo" {
     Name = "TF-Instance"
   }
 }
-resource "aws_security_group" "allow_all" {
-  name        = "allow_all"
-  description = "Allow all inbound traffic"
-
-  ingress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
