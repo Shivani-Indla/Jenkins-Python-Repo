b7e801df616e11880f67fb806ef2353d2d3efcf3
Shivani-Indla <shiva.indla1518@gmail.com>
2025-04-22 02:06:14 +0530

    My 5th commit to create ec2-instance using jenkins pipeline with terraform code

--- main.tf
+++ main.tf
@@ -4,7 +4,16 @@ provider "aws" {
 resource "aws_instance" "foo" {
   ami           = "ami-0e449927258d45bc4"
   instance_type = "t2.micro"
-
+  key_name      = "SRK-Key"
+  subnet_id     = "subnet-0da5d1190995a43ed"
+  vpc_security_group_ids = ["sg-093350e2375367437"]
+  associate_public_ip_address = true
+  user_data = <<-EOF
+              #!/bin/bash
+              echo "Hello, World!" > /var/www/html/index.html
+              systemctl start httpd
+              systemctl enable httpd
+              EOF
   tags = {
     Name = "TF-Instance"
   }
